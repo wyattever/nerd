2bde26c11de50fc25521abacdf6c2381d0a2ebf8
Wyatt Ever <wyattever@gmail.com>
2026-06-26 16:28:01 -0600

    feat(section-editor): Sanitize HTML with DOMPurify

--- frontend/components/ListingCard.tsx
+++ frontend/components/ListingCard.tsx
@@ -1,13 +1,15 @@
+import DOMPurify from "dompurify";
 import { ListingData } from "@/lib/types";
 import { buildNcademiListingHtml } from "@/lib/ncademiPreview";
 
 export function ListingCard({ listing }: { listing: ListingData }) {
   const html = buildNcademiListingHtml(listing);
+  const safeHtml = DOMPurify.sanitize(html, { USE_PROFILES: { html: true } });
 
   return (
     <div
       className="ncademi-listing-preview"
-      dangerouslySetInnerHTML={{ __html: html }}
+      dangerouslySetInnerHTML={{ __html: safeHtml }}
     />
   );
 }
